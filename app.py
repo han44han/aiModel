52c9971df5bf8aba2f0db20637b11c0a8eface5b
han44han <hm831063@gmail.com>
2026-05-08 15:27:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from pydantic import BaseModel
 from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
 from detectron2 import model_zoo
-from huggingface_hub import hf_hub_download  # مكتبة التحميل الجديدة
+
 
 app = FastAPI()
 
